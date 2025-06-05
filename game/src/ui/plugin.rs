16e7da40ca555affb6c5bb0ea76ccf603c3eb4d8
kaiva-morphin <kaivamorphin@gmail.com>
2025-06-06 00:26:49 +0300

    ui components

--- game/src/ui/plugin.rs
+++ game/src/ui/plugin.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
+use bevy_asset_loader::{asset_collection::AssetCollection, loading_state::{config::{ConfigureLoadingState, LoadingStateConfig}, LoadingStateAppExt}};
 
-use crate::ui::components::hack_button::ui_hack_button_hover;
+use crate::{core::states::AppLoadingAssetsSubState, ui::components::hack_button::ui_hack_button_hover};
 
 
 
@@ -10,7 +11,21 @@ pub struct UiSystemPlugin;
 impl Plugin for UiSystemPlugin {
     fn build(&self, app: &mut App) {
         app
+            .configure_loading_state(
+                LoadingStateConfig::new(AppLoadingAssetsSubState::Loading)
+                    .load_collection::<UiAssetCollection>(),
+            )
             .add_systems(Update, ui_hack_button_hover)
         ;
     }
+}
+
+#[derive(AssetCollection, Resource)]
+pub struct UiAssetCollection {
+    #[asset(path = "ui/hack_button.png")]
+    pub button: Handle<Image>,
+    #[asset(path = "ui/main_container.png")]
+    pub main_container: Handle<Image>,
+    #[asset(path = "ui/sub_container.png")]
+    pub sub_container: Handle<Image>,
 }
